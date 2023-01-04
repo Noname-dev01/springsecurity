83d136fccd23c62af314477e3cab01ae1abc6f43
noname_dev <noname_dev@naver.com>
2023-01-04 23:03:56 +0900

    logout 처리, LogoutFilter

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -9,10 +9,13 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -26,7 +29,7 @@ public class SecurityConfig {
                 .anyRequest().authenticated();
         http
                 .formLogin()
-                .loginPage("/loginPage") //사용자 정의 로그인 페이지
+//                .loginPage("/loginPage") //사용자 정의 로그인 페이지
                 .defaultSuccessUrl("/") // 로그인 성공 후 이동 페이지
                 .failureUrl("/login") //로그인 실패 후 이동 페이지
                 .usernameParameter("userId") //아이디 파라미터명 설정
@@ -47,6 +50,25 @@ public class SecurityConfig {
                     }
                 }) //로그인 실패 후 핸들러
                 .permitAll();
+
+        http
+                .logout() //로그아웃 처리
+                .logoutUrl("/logout") //로그아웃 처리 Url
+                .logoutSuccessUrl("/login") //로그아웃 성공 후 이동페이지
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                }) //로그아웃 핸들러
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                }) //로그아웃 성공 후 핸들러
+                .deleteCookies("remember-me"); //로그아웃 후 쿠키 삭제
 
         return http.build();
     }
